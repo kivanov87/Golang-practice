e647c7749a424d1e2d14871f59c3d71f71e9f605
kivanov87 <kalin.at.ivanov@gmail.com>
2024-02-16 21:25:37 +0200

    new

--- Exercise/Exercise-CS/1. Sum Seconds/app.go
+++ Exercise/Exercise-CS/1. Sum Seconds/app.go
@@ -1,5 +1,19 @@
 package main
 
-func main () {
-	
+import "fmt"
+
+func main() {
+	var time1, time2, time3 int
+	fmt.Scanln(&time1)
+	fmt.Scanln(&time2)
+	fmt.Scanln(&time3)
+
+	sumTime := time1 + time2 + time3
+
+	var minutes int = sum / 60 
+	seconds := sum % 60
+
+	if seconds < 10 {
+		fmt.Printf()
+	}
 }
---
+++ Exercise/Exercise-CS/3.TimePlus15Minutes/app.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var timeHour, timeMinutes int
+	fmt.Scanln(&timeHour)
+	fmt.Scanln(&timeMinutes)
+
+	
+
+
+}
